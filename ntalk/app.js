115179bc3fb1e26b49f086f33c8f718ca8c22464
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-10-08 22:02:24 +0200

    Implement express-load

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,13 +1,15 @@
 var express = require('express')
-  , routes = require('./routes/index')
-  , users = require('./routes/users')
+  , load = require('express-load')
   , app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use('/', routes);
-app.use('/usuarios', users);
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
 app.listen(3000, function(){
   console.log("Ntalk no ar.");
