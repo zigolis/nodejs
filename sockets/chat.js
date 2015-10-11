656f629c5e70e3802082cac408212c4ed4768e0b
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-10-11 22:27:29 +0200

    Implement status notification

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,17 @@
 module.exports = function(io) {
   var crypto = require('crypto')
-    , sockets = io.sockets;
+    , sockets = io.sockets
+    , onlines = {};
 
   sockets.on('connection', function(client) {
     var session = client.handshake.session
       , usuario = session.usuario;
+
+    onlines[usuario.email] = usuario.email;
+    for (var email in onlines) {
+      client.emit('notify-onlines', email);
+      client.broadcast.emit('notify-onlines', email);
+    }
 
     client.on('join', function(sala) {
       if(!sala) {
@@ -21,14 +28,6 @@ module.exports = function(io) {
       sockets.in(sala).emit('send-client', msg);
     });
 
-    client.on('disconnect', function () {
-      var sala = session.sala
-        , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
-
-      sockets.in(sala).emit('send-client', msg);
-      client.leave(session.sala);
-    });
-
     client.on('send-server', function (msg) {
       var sala = session.sala
           , data = {email: usuario.email, sala: sala};
@@ -36,6 +35,16 @@ module.exports = function(io) {
 
       client.broadcast.emit('new-message', data);
       sockets.in(sala).emit('send-client', msg);
+    });
+
+    client.on('disconnect', function () {
+      var sala = session.sala
+        , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+
+      client.broadcast.emit('notify-offlines', usuario.email);
+      sockets.in(sala).emit('send-client', msg);
+      delete onlines[usuario.email];
+      client.leave(session.sala);
     });
   });
 }
