a48ce06ea31233a75a3ae77d831eaa0075f65e4c
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-10-11 21:38:03 +0200

    Implement one 2 one chat

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,15 +1,41 @@
 module.exports = function(io) {
-  var sockets = io.sockets;
+  var crypto = require('crypto')
+    , sockets = io.sockets;
 
   sockets.on('connection', function(client) {
     var session = client.handshake.session
       , usuario = session.usuario;
 
-    client.on('send-server', function(msg) {
-      msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
+    client.on('join', function(sala) {
+      if(!sala) {
+        var timestamp = new Date().toString()
+          , md5 = crypto.createHash('md5');
 
-      client.emit('send-client', msg);
-      client.broadcast.emit('send-client', msg);
+        sala = md5.update(timestamp).digest('hex');
+      }
+
+      session.sala = sala;
+      client.join(sala);
+
+      var msg = "<b>"+usuario.nome+":</b> entrou.<br>";
+      sockets.in(sala).emit('send-client', msg);
+    });
+
+    client.on('disconnect', function () {
+      var sala = session.sala
+        , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+
+      sockets.in(sala).emit('send-client', msg);
+      client.leave(session.sala);
+    });
+
+    client.on('send-server', function (msg) {
+      var sala = session.sala
+          , data = {email: usuario.email, sala: sala};
+      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+
+      client.broadcast.emit('new-message', data);
+      sockets.in(sala).emit('send-client', msg);
     });
   });
 }
