6c86e00167a86f7ab1579d1ac95c5f8281d7ad96
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-10-15 01:29:49 +0200

    Add redis to persist chat history

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,17 +1,20 @@
 module.exports = function(io) {
   var crypto = require('crypto')
-    , sockets = io.sockets
-    , onlines = {};
+    , redis = require('redis').createClient()
+    , sockets = io.sockets;
 
   sockets.on('connection', function(client) {
     var session = client.handshake.session
       , usuario = session.usuario;
 
-    onlines[usuario.email] = usuario.email;
-    for (var email in onlines) {
-      client.emit('notify-onlines', email);
-      client.broadcast.emit('notify-onlines', email);
-    }
+    redis.sadd('onlines', usuario.email, function(erro) {
+      redis.smembers('onlines', function(erro, emails) {
+        emails.forEach(function(email) {
+          client.emit('notify-onlines', email);
+          client.broadcast.emit('notify-onlines', email);
+        });
+      });
+    });
 
     client.on('join', function(sala) {
       if(!sala) {
@@ -25,26 +28,36 @@ module.exports = function(io) {
       client.join(sala);
 
       var msg = "<b>"+usuario.nome+":</b> entrou.<br>";
-      sockets.in(sala).emit('send-client', msg);
-    });
 
-    client.on('send-server', function (msg) {
-      var sala = session.sala
-          , data = {email: usuario.email, sala: sala};
-      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
-
-      client.broadcast.emit('new-message', data);
-      sockets.in(sala).emit('send-client', msg);
+      redis.lpush(sala, msg, function(erro, res) {
+        redis.lrange(sala, 0, -1, function(erro, msgs) {
+          msgs.forEach(function(msg) {
+            sockets.in(sala).emit('send-client', msg);
+          });
+        });
+      });
     });
 
     client.on('disconnect', function () {
       var sala = session.sala
         , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
 
+      redis.lpush(sala, msg);
       client.broadcast.emit('notify-offlines', usuario.email);
       sockets.in(sala).emit('send-client', msg);
-      delete onlines[usuario.email];
+      redis.srem('onlines', usuario.email);
       client.leave(session.sala);
+    });
+
+    client.on('send-server', function (msg) {
+      var sala = session.sala
+        , data = {email: usuario.email, sala: sala};
+
+      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+
+      redis.lpush(sala, msg);
+      client.broadcast.emit('new-message', data);
+      sockets.in(sala).emit('send-client', msg);
     });
   });
 }
